f20abf1155f8b91f82790f9f0723b858bfd9124f
Vardhan Rajnikant Shah <vardhan.s@ahduni.edu.in>
2021-11-01 22:44:33 +0530

    Functions .

---
+++ my.js
@@ -0,0 +1,14 @@
+//alert("Hello World! , succesfully Loaded JS file.");
+document.getElementById("demo").innerHTML = f(1,7);
+document.getElementById("demo").innerHTML = f(1,9);
+
+console.log(f);
+
+
+
+
+
+function f(a,b){
+
+    return a+b;
+}
